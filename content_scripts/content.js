71a7e67ec1d5c36057ebb8727ad17d111be450d7
haraldchan <katahane42@gmail.com>
2024-04-02 21:10:33 +0800

    added getFormattedDateTime function

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,6 +1,17 @@
 const identifier = '04047fce826f48f751891b4721f7ac70' // MD5 hash: ProfileModifyNext
 const newGuestModalParent = document.querySelector('body')
 
+function getFormattedDateTime() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+}
+
 const observer = new MutationObserver(async (mutationsList, observer) => {
     for (let mutation of mutationsList) {
         if (mutation.type === 'childList') {
@@ -23,9 +34,10 @@ const observer = new MutationObserver(async (mutationsList, observer) => {
 					guestInfo.gender = genderLabel.nextElementSibling.getElementsByTagName('input')[0].value
 					guestInfo.birthday = birthdayLabel.nextElementSibling.getElementsByTagName('input')[0].value
 					guestInfo.address = addrLabel.nextElementSibling.getElementsByTagName('input')[0].value
-					guestInfo.idType = idTypeLabel.nextElementSibling.getElementsByTagName('input')[0].value
+					guestInfo.id3Type = idTypeLabel.nextElementSibling.getElementsByTagName('input')[0].value
 					guestInfo.idNum = idNumLabel.nextElementSibling.getElementsByTagName('input')[0].value
 					guestInfo.roomNum = roomNumLabel.nextElementSibling.getElementsByTagName('input')[0].value
+					guestInfo.loggedTime = getFormattedDateTime()
 
 					console.log(guestInfo)
 					navigator.clipboard.writeText(JSON.stringify(guestInfo))
