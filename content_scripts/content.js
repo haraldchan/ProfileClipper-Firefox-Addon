f1c07096259069293d833bb718b12b763815d2a9
haraldchan <116058185+haraldchan@users.noreply.github.com>
2024-04-07 19:29:44 +0800

    wip

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,5 +1,5 @@
 const identifier = '04047fce826f48f751891b4721f7ac70' // MD5 hash: ProfileModifyNext
-const newGuestModalParent = document.querySelector('body')
+const body = document.querySelector('body')
 const fieldLabelPatterns = {
 	mainland: {
 		name: 'xm',
@@ -32,27 +32,14 @@ const fieldLabelPatterns = {
 	},
 }
 
-function getFormattedDateTime() {
-	const date = new Date()
-	const year = date.getFullYear()
-	const month = String(date.getMonth() + 1).padStart(2, '0')
-	const day = String(date.getDate()).padStart(2, '0')
-	const hours = String(date.getHours()).padStart(2, '0')
-	const minutes = String(date.getMinutes()).padStart(2, '0')
-	const seconds = String(date.getSeconds()).padStart(2, '0')
-	return `${year}${month}${day}${hours}${minutes}${seconds}`
-}
-
 function cleanLocalStorage() {
 	const currentTime = new Date().getTime()
 	for (let i = 0; i < localStorage.length; i++) {
 		const key = localStorage.key(i)
-		const timestamp = parseInt(key, 10) // Parse the key as an integer
+		const timestamp = parseInt(key, 10)
 		if (!isNaN(timestamp)) {
-			// Check if the key is a valid timestamp
 			if (currentTime - timestamp >= 24 * 60 * 60 * 1000) {
-				// Check if the timestamp is older than 24 hours
-				localStorage.removeItem(key) // Remove the item from local storage
+				localStorage.removeItem(key)
 			}
 		}
 	}
@@ -85,9 +72,11 @@ function getGuestInfo(guestType) {
 }
 
 const observer = new MutationObserver(async (mutationsList, observer) => {
+	const newGuestModal = document.querySelector('.el-dialog__wrapper')
+	const modalStatus = window.getComputedStyle(newGuestModal).getPropertyValue('display')
+
 	for (let mutation of mutationsList) {
-		if (mutation.type === 'childList') {
-			// when button appear means the modal is show
+		if (mutation.type === 'childList' && modalStatus !== 'none') {
 			const span = Array.from(document.getElementsByTagName('span'))
 			const submitBtn = span.filter((span) => span.innerText === '上报(R)')[0].parentElement
 			const guestTypes = Array.from(span.filter((span) => span.innerText === '内地旅客')[0].parentElement.parentElement.querySelectorAll('.el-radio'))
@@ -95,14 +84,16 @@ const observer = new MutationObserver(async (mutationsList, observer) => {
 			if (!submitBtn.hasAttribute('capture-event-added')) {
 				submitBtn.addEventListener('click', () => {
 					const currentGuestType = guestTypes.filter((radio) => radio.classList.contains('is-checked'))[0].textContent
-					console.log(currentGuestType)
-
 					const guestInfo = getGuestInfo(currentGuestType)
-					console.log(guestInfo)
 
+					Object.values(guestInfo).forEach(val => {
+						if (val === "") return
+					})
+
+					console.log(guestInfo)
 					navigator.clipboard.writeText(JSON.stringify(guestInfo))
 					localStorage.setItem(new Date().getTime(), JSON.stringify(guestInfo))
-					// cleanLocalStorage()
+					cleanLocalStorage()
 				})
 
 				document.addEventListener('keyup', (e) => {
@@ -116,4 +107,4 @@ const observer = new MutationObserver(async (mutationsList, observer) => {
 	}
 })
 
-observer.observe(newGuestModalParent, { childList: true })
+observer.observe(body, { childList: true })
